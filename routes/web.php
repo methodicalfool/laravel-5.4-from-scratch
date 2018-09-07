63417460b778aa02310b578ffde14a74ebd2b582
Jeremy O'Reilly <jeremy@isparks.com>
2018-09-07 07:44:24 -0600

    From the laptop. Currently working.
    At about 2.5 minutes into this lesson:
    https://laracasts.com/series/laravel-from-scratch-2017/episodes/7?autoplay=true
    
    (Previous commit was really at the start of this video)

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,21 @@
 |
 */
 
+use App\Task;
+
 Route::get('/tasks', function () {
-    $tasks = DB::table('tasks')->get();
+    #$tasks = DB::table('tasks')->get();
+    $tasks = Task::all();
     #return $tasks;
     return view('tasks.index')->with( compact('tasks') );
 });
 
 
 Route::get('/task/{task}', function($id){
-    $task = DB::table('tasks')->find($id);
+    #$task = DB::table('tasks')->find($id);
+    $task = Task::find($id);
+    #$task = Task::whereKey($id);
+    #return $task;
     return view('tasks.show')->with( compact('task') );
     #return view('about');
 });
